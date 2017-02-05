71bbae06eda7a7a0e62efcd07336499610c5a390
Catherine Taylor <cat.taylor.taylor@btinternet.com>
2017-02-05 12:15:26 +0000

    Finished moves and constructors

--- piece.cpp
+++ piece.cpp
@@ -1,41 +1,45 @@
 #include "piece.hpp"
+#include <iostream>     // std::cout
+#include <cmath>
 
 void piece::hello_piece_world(void){
     cout<<"black = ♔♕♖♗♘♙\nwhite = ♚♛♜♝♞♟\n";
 };
 
 
-pawn::pawn(){
-    current_x = 0;
-    current_y=1;
+pawn::pawn(int x_coord, int y_coord, int colour){
+    current_x = x_coord;
+    current_y=y_coord;
+    team_colour = colour;
     is_initial=1;
 };
 void pawn::move(int new_x, int new_y){
      if (is_initial ==1){
-        if ((new_x == current_x)&&((new_y==current_y+1)|(new_y=current_y+2))){
+        if ((new_x == current_x)&&((new_y==current_y+1*team_colour)|(new_y==current_y+2*team_colour))){
             current_x=new_x;
             current_y=new_y;
             is_initial=0;
         return;
         };
     };
-    if ((new_x == current_x)&&(new_y==current_y+1)){
+    if ((new_x == current_x)&&(new_y==current_y+1*team_colour)){
         current_x=new_x;
         current_y=new_y;
         return;
-    }
+    };
     cout<<"Invalid Move"<<'\n';
     return;
     };
 
-king::king(){
-  //  if WHITE {
+king::king(int colour){
+    team_colour=colour;
+   if(team_colour== WHITE){
         current_x= 3;
         current_y= 0;
-    //return;
-    //};
-      //  current_x= 3;
-        //current_y= 7;
+        return;
+    };
+       current_x= 3;
+       current_y= 7;
 };
 
 void king::move( int new_x, int new_y){
@@ -43,32 +47,27 @@ void king::move( int new_x, int new_y){
             cout<<"Invalid Move"<<'\n';
     return;
     };
-    if ((new_x <0)|(new_x >7)|(new_y<0)|(new_y>7)){
-            cout<<"Invalid Move"<<'\n';
-        return;
-    };
     current_x=new_x;
     current_y=new_y;
         return;
 };
 
- queen::queen(){
-// if WHITE{
+ queen::queen(int colour){
+     team_colour=colour;
+ if (colour==WHITE){
   current_x=4;
      current_y=0;
-//     return;
-//    };
-//  current_x =4;
-//     current_y=7;
+     
+    return;
+   };
+    current_x =4;
+    current_y=7;
+  
  };
 
 
 void queen::move(int new_x, int new_y){
-    if ((new_x <0)|(new_x >7)|(new_y<0)|(new_y>7)){
-                cout<<"Invalid Move"<<'\n';
-            return;
-    };
-    if ((current_x - new_x !=0) && (current_y-new_y!=0)&&((current_x-new_x)*(current_x-new_x)!=(current_y-new_y)*(current_y-new_y))){
+      if ((current_x - new_x !=0) && (current_y-new_y!=0)&&((abs(current_x-new_x))!=(abs(current_y-new_y)))){
                 cout<<"Invalid Move" << '\n';
             return;
         };
@@ -77,9 +76,10 @@ void queen::move(int new_x, int new_y){
         return;
 };
 
-rook::rook(){
-current_x=7;
-current_y=0;
+rook::rook(int x_coord, int y_coord, int colour){
+current_x=x_coord;
+current_y=y_coord;
+team_colour = colour;
 };
 
 void rook::move(int new_x, int new_y){
@@ -87,18 +87,15 @@ void rook::move(int new_x, int new_y){
         cout<<"Invalid Move"<<'\n';
         return;
     };
-    if ((new_x <0)|(new_x >7)|(new_y<1)|(new_y>8)){
-            cout<<"Invalid Move"<<'\n';
-        return;
-    };
         current_x=new_x;
         current_y=new_y;
     return;
 };
 
-bishop::bishop(){
-    current_x=2;
-    current_y=0;
+bishop::bishop(int x_coord, int y_coord, int colour){
+    current_x=x_coord;
+    current_y=y_coord;
+    team_colour=colour;
 };
 
 void bishop::move(int new_x, int new_y){
@@ -106,7 +103,7 @@ void bishop::move(int new_x, int new_y){
         cout<<"Invalid Move"<<'\n';
         return;
     };
-    if ((current_x - new_x !=0) && (current_y-new_y!=0)&&((current_x-new_x)*(current_x-new_x)!=(current_y-new_y)*(current_y-new_y))){
+    if ((current_x - new_x !=0) && (current_y-new_y!=0)&&(abs(current_x-new_x)!=abs(current_y-new_y))){
             cout<<"Invalid Move" << '\n';
         return;
     };    
@@ -119,16 +116,14 @@ void bishop::move(int new_x, int new_y){
     return;
 };
 
-knight::knight(){
-    current_x = 1;
-    current_y=0;
+knight::knight(int x_coord, int y_coord, int colour){
+    current_x = x_coord;
+    current_y=y_coord;
+    team_colour = colour;
 };
 
 void knight::move(int new_x, int new_y){
-    if ((new_x <0)|(new_x >7)|(new_y<0)|(new_y>7)){
-            cout<<"Invalid Move"<<'\n';
-        return;
-    };
+  
     if ((new_x == current_x - 1)&&((new_y == current_y -2)|(new_y == current_y+2))){
             current_x=new_x;
             current_y=new_y;
@@ -152,4 +147,5 @@ void knight::move(int new_x, int new_y){
     cout<<"Invalid Move"<<'\n';
     return;
     };
-
+    
+    

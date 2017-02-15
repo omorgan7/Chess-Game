da2d80317fa0deeff50b9c1756654654a1965848
Owen Morgan <o.morgan@bath.ac.uk>
2017-02-15 13:04:58 +0000

    Fleshed out king search area.

--- game.hpp
+++ game.hpp
@@ -14,7 +14,9 @@ class game {
         void display_board_state(void);
         void print_chessboard_line(string *statestring);
         void process_input(int color);
+        bool Check(int color);
         bool CheckMate(int color);
+        bool SearchKingSpace(int color);
 		bool update_board_state(string move, int colour);
         void initialiseKingPosition(void);
 		void reset();
@@ -27,6 +29,7 @@ class game {
         string black_pawns;
         int black_king_index;
         int white_king_index;
+        int king_index;
 
 };
 

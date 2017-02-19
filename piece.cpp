949223629858e3b934c15efa25d2e7dd19c87b2f
Catherine Taylor <cat.taylor.taylor@btinternet.com>
2017-02-19 16:52:51 +0000

    refactor moves

--- piece.cpp
+++ piece.cpp
@@ -23,13 +23,11 @@ int piece::get_is_initial(void){
     return is_initial;
 }
 int piece::space_free(int new_x, int new_y, piece **board){
-    if (board[new_x + 8*new_y] == nullptr){
-        return 1;
-    }
+    if (board[new_x + 8*new_y] != nullptr){
     if (board[new_x + 8*new_y] -> getColour()==team_colour){
         return 0;
     }
-    return -1;
+    }return -1;
 }
 int piece::line_search_diagonal(int new_x, int new_y, piece **board){
     if (((abs(c_x + 8*c_y -  new_x - 8*new_y))%7==0)|((abs(c_x + 8*c_y -  new_x - 8*new_y))%9==0)){
@@ -69,20 +67,17 @@ void piece::not_initial(void){
 //Pawn Class
 pawn::pawn(int x_coord, int y_coord, int colour){
     basic(x_coord,y_coord, colour);
-        piece_value = pawn_value;
+    piece_value = pawn_value;
     if (team_colour == WHITE){
         PieceName = "WP";
-    return;
     }
+    else{
     PieceName = "BP";
-};
+    }
+}
 bool pawn::initial_move(int new_x, int new_y, piece **board){ 
-    if (is_initial!=1){
-        return 0;
-    }  
-     if ((new_x == c_x)&&((new_y==c_y+1*team_colour)|(new_y==c_y+2*team_colour))){
-            is_initial=0;
-            return 1;
+    if ((new_x == c_x)&&(((new_y-c_y)==(1*team_colour))|((new_y-c_y)==(2*team_colour)))){
+        return 1;
     }
     return 0;
 }
@@ -96,12 +91,8 @@ bool pawn::move(int new_x, int new_y, piece **board){
         }
         return 0;
     }
-
     if (is_initial==1){
-        if ((new_x == c_x)&&(((new_y-c_y)==(1*team_colour))|((new_y-c_y)==(2*team_colour)))){
-            return 1;
-        }
-        return 0;
+        return pawn::initial_move(new_x, new_y, board);
     }
     if ((new_x == c_x)&&((new_y-c_y)==(1*team_colour))){
         return 1;
@@ -171,45 +162,12 @@ bool queen::move(int new_x, int new_y, piece **board){
         if (board[new_x + 8*new_y] -> getColour()==team_colour){
             return 0;
         }
-        if (((new_x==c_x)&&(new_y!=c_y))|((new_y==c_y)&&(new_x!=c_x))){
-            int it =(new_x==c_x)*8*pow(-1, new_y<c_y)+(new_y==c_y)*pow(-1, new_x<c_x);
-            for (int i = c_x + 8*c_y+it; i != new_x + 8*new_y; i+=it){
-                if (board[i] != nullptr){
-                    return 0;
-                }
-            } 
-            return 1;
-        }
-        if (((abs(c_x + 8*c_y -  new_x - 8*new_y))%7==0)|((abs(c_x + 8*c_y -  new_x - 8*new_y))%9==0)){
-            int it = 8*pow(-1,new_y<c_y);
-            it = it+pow(-1,new_x<c_x); 
-            for (int i = c_x + 8*c_y+it; i != new_x + 8*new_y; i+=it){
-                if (board[i] != nullptr){
-                    return 0;
-                }
-            } 
-                return 1;  
-        }
-            return 0;
     }
     if (((new_x==c_x)&&(new_y!=c_y))|((new_y==c_y)&&(new_x!=c_x))){
-        int it =(new_x==c_x)*8*pow(-1, new_y<c_y)+(new_y==c_y)*pow(-1, new_x<c_x);
-        for (int i = c_x + 8*c_y+it; i != new_x + 8*new_y; i+=it){
-            if (board[i] != nullptr){
-                return 0;
-            }
-        }
-        return 1;  
+        return piece::line_search_straight(new_x, new_y, board);
     }
     if (((abs(c_x + 8*c_y -  new_x - 8*new_y))%7==0)|((abs(c_x + 8*c_y -  new_x - 8*new_y))%9==0)){
-        int it = 8*pow(-1,new_y<c_y);
-        it = it+pow(-1,new_x<c_x); 
-        for (auto i = c_x + 8*c_y+it; i != new_x + 8*new_y; i+=it){
-            if (board[i] != nullptr){
-                return 0;
-            }
-        } 
-        return 1;  
+        return piece::line_search_diagonal(new_x, new_y, board);
     }
     return 0;
 }
@@ -223,31 +181,15 @@ rook::rook(int x_coord, int y_coord, int colour){
         return;
     }
     PieceName = "BR";
-};
+}
 bool rook::move(int new_x, int new_y, piece **board){
     if (board[new_x + 8*new_y] != nullptr){
         if (board[new_x + 8*new_y] -> getColour()==team_colour){
             return 0;
-        }  
-        if (((new_x==c_x)&&(new_y!=c_y))|((new_y==c_y)&&(new_x!=c_x))){
-            int it =(new_x==c_x)*8*pow(-1, new_y<c_y)+(new_y==c_y)*pow(-1, new_x<c_x);
-            for (auto i = c_x + 8*c_y+it; i != new_x + 8*new_y; i+=it){
-                if (board[i] != nullptr){
-                    return 0;
-                }
-            } 
-            return 1;
         }
-        return 0;
-    }
+    } 
     if (((new_x==c_x)&&(new_y!=c_y))|((new_y==c_y)&&(new_x!=c_x))){
-        int it =(new_x==c_x)*8*pow(-1, new_y<c_y)+(new_y==c_y)*pow(-1, new_x<c_x);
-        for (auto i = c_x + 8*c_y+it; i != new_x + 8*new_y; i+=it){
-            if (board[i] != nullptr){
-                return 0;
-            }
-        }
-        return 1;
+        return piece::line_search_straight(new_x, new_y, board);
     }
     return 0;
 }
@@ -261,38 +203,18 @@ bishop::bishop(int x_coord, int y_coord, int colour){
         return;
     }
     PieceName ="BB";
-};
+}
 bool bishop::move(int new_x, int new_y, piece **board){   
     if (board[new_x + 8*new_y] != nullptr){
         if (board[new_x + 8*new_y] -> getColour()==team_colour){
             return 0;
         }
-        if (((abs(c_x + 8*c_y -  new_x - 8*new_y))%7==0)|((abs(c_x + 8*c_y -  new_x - 8*new_y))%9==0)){
-            int it = 8*pow(-1,(new_y<c_y));
-            it = it+pow(-1,(new_x<c_x));      
-            for (auto i = c_x + 8*c_y+it; i != new_x + 8*new_y; i+=it){
-                if (board[i] != nullptr){
-                    return 0;
-                }
-            }
-            return 1;
-        }
-        return 0;
     }
     if (((abs(c_x + 8*c_y -  new_x - 8*new_y))%7==0)|((abs(c_x + 8*c_y -  new_x - 8*new_y))%9==0)){
-        int it = 8*pow(-1,new_y<c_y);
-        it = it+pow(-1,new_x<c_x); 
-        for (auto i = c_x + 8*c_y+it; i != new_x + 8*new_y; i+=it){
-            if (board[i] != nullptr){
-                return 0;
-            }
-        }
-        return 1;
+        return piece::line_search_diagonal(new_x, new_y, board);
     }   
     return 0;
-};
-
-
+}
 /////////////////
 //Knight Class
 knight::knight(int x_coord, int y_coord, int colour){
@@ -310,20 +232,18 @@ bool knight::move(int new_x, int new_y, piece **board){
                 return 0;
             }
     }
-
     if ((new_x == c_x - 1)&&((new_y == c_y -2)|(new_y == c_y+2))){
         return 1;
-    }  ;
+    } 
     if ((new_x == c_x - 2)&&((new_y == c_y -1)|(new_y == c_y+1))){
         return 1;
-    };
+    }
     if ((new_x == c_x + 1)&&((new_y == c_y -2)|(new_y == c_y+2))){
         return 1;
-    }  ;
+    } 
     if ((new_x == c_x + 2)&&((new_y == c_y -1)|(new_y == c_y+1))){
         return 1;
-    };
-
+    }
     return 0;
-}; 
+}
 
